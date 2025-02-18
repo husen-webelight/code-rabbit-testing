008f3aefd3cf704e38dae6ff0e3400ad7692a141
Husen <jivanig@webelight.co.in>
2025-02-18 16:27:46 +0530

    Add initial project structure and configuration files to test code rabbit ai tool

---
+++ android/app/src/main/kotlin/com/fitness/app/fitness_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.fitness.app.fitness_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/const/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color bgColor = Color(0xC2221F1F);
+  static const Color transparent = Colors.transparent;
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color blue = Color(0xFF00B3FF);
+}
